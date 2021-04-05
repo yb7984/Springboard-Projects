b47cf71ee4180ba8c657e17db8cfa55afed23694
Sunbao Wu <bobowu@outlook.com>
2021-04-05 07:20:30 -0400

    adding 35.3 express-lunchly

---
+++ 35.3-express-lunchly/models/customer.js
@@ -0,0 +1,137 @@
+/** Customer for Lunchly */
+
+const db = require("../db");
+const Reservation = require("./reservation");
+
+/** Customer of the restaurant. */
+
+class Customer {
+  constructor({ id, firstName, lastName, phone, notes }) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.notes = notes;
+  }
+
+
+  /**
+   * Return the full name of the customer
+   * @returns firstName lastName
+   */
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  /** find all customers. */
+
+  static async all() {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes
+       FROM customers
+       ORDER BY last_name, first_name`
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+
+  /** search customers. */
+
+  static async search(term) {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes
+      FROM customers
+      WHERE
+      LOWER(first_name) LIKE $1 OR Lower(last_name) LIKE $1
+      ORDER BY last_name, first_name` , [`%${term.toLowerCase()}%`]
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+
+  /** top 10 customers ordered by most reservations. */
+
+  static async best() {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes
+      FROM customers
+      JOIN
+        (
+          SELECT 
+            customer_id , COUNT(*) AS count 
+          FROM reservations 
+          GROUP BY customer_id 
+          ORDER BY COUNT(*) DESC 
+          LIMIT 10
+        ) AS res_count
+      ON 
+        id = customer_id
+      ORDER BY count DESC`
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+  /** get a customer by ID. */
+
+  static async get(id) {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes 
+        FROM customers WHERE id = $1`,
+      [id]
+    );
+
+    const customer = results.rows[0];
+
+    if (customer === undefined) {
+      const err = new Error(`No such customer: ${id}`);
+      err.status = 404;
+      throw err;
+    }
+
+    return new Customer(customer);
+  }
+
+  /** get all reservations for this customer. */
+
+  async getReservations() {
+    return await Reservation.getReservationsForCustomer(this.id);
+  }
+
+  /** save this customer. */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO customers (first_name, last_name, phone, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.firstName, this.lastName, this.phone, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
+             WHERE id=$5`,
+        [this.firstName, this.lastName, this.phone, this.notes, this.id]
+      );
+    }
+  }
+}
+
+module.exports = Customer;
---
+++ 35.3-express-lunchly/models/reservation.js
@@ -0,0 +1,93 @@
+/** Reservation for Lunchly */
+
+const moment = require("moment");
+
+const db = require("../db");
+
+
+/** A reservation for a party */
+
+class Reservation {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
+    this.id = id;
+    this.customerId = customerId;
+    this.numGuests = numGuests;
+    this.startAt = startAt;
+    this.notes = notes;
+  }
+
+  /** getter for numGuests */
+  get numGuests(){
+    return this._numGuests;
+  }
+
+  /** setter for numGuest */
+  set numGuests(val){
+    if (val === undefined){
+      throw Error("Number of Guests is required");
+    }
+
+    if (parseInt(val) < 1){
+      throw Error("Number of Guest must be equal or greater than 1");
+    }
+
+    this._numGuests = parseInt(val);
+  }
+
+  /** getter for startAt */
+  get startAt(){
+    return this._startAt;
+  }
+
+  /** setter for startAt */
+  set startAt(val){
+    this._startAt = new Date(val);
+  }
+
+  /** formatter for startAt */
+
+  getformattedStartAt() {
+    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+  }
+
+  /** given a customer id, find their reservations. */
+
+  static async getReservationsForCustomer(customerId) {
+    const results = await db.query(
+      `SELECT id, 
+           customer_id AS "customerId", 
+           num_guests AS "numGuests", 
+           start_at AS "startAt", 
+           notes AS "notes"
+         FROM reservations 
+         WHERE customer_id = $1`,
+      [customerId]
+    );
+
+    return results.rows.map(row => new Reservation(row));
+  }
+
+
+  /** save this reservation. */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customerId, this.startAt, this.numGuests, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE reservations SET start_at=$1, num_guests=$2, notes=$3
+             WHERE id=$4`,
+        [this.startAt, this.this.numGuests, this.notes, this.id]
+      );
+    }
+  }
+}
+
+
+module.exports = Reservation;
